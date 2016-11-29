c69e2dc5b97b31ecdaba006a69716499c8e3fd3a
Nico Gensheimer <nico-gensheimer@gmx.de>
2016-11-29 10:28:16 +0100

    same as player
    same as player

---
+++ src/oot/game/Highscore.java
@@ -0,0 +1,28 @@
+package src.oot.game;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class Highscore {
+
+	/**
+	 * @author Nico Gensheimer
+	 * @param
+	 * @throws IOException
+	 */
+
+	//Pfad zum speichern des Highscroes
+	private String path = "C:/highscore.txt";
+
+	//Speichert name und highscore des übergebendem Spielers
+	public void saveHighscore(Player player) throws IOException
+	{
+		FileOutputStream ioStream = new FileOutputStream(path);
+		ObjectOutputStream objStream = new ObjectOutputStream(ioStream);
+		objStream.writeObject(player.getName()+" "+player.getHighscore());
+		objStream.close();
+		ioStream.close();
+	}
+
+}
