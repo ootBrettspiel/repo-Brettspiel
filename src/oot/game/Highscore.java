db22013f10573c5b4eb90df6ff1de22ba0a55f70
Nico Gensheimer <nico-gensheimer@gmx.de>
2016-11-30 13:25:03 +0100

    Save & Print
    Filewrite ansatt Outputstream benutzt.
    Ausgeben des Highscores hinzugefügt.

--- src/oot/game/Highscore.java
+++ src/oot/game/Highscore.java
@@ -1,28 +1,64 @@
 package oot.game;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 
 public class Highscore {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		Highscore x = new Highscore();
+		x.print();
+		x.printHighscore();
+	}
 
 	/**
 	 * @author Nico Gensheimer
 	 * @param
 	 * @throws IOException
 	 */
+	
+	
 
-	//Pfad zum speichern des Highscroes
-	private String path = "C:/highscore.txt";
 
-	//Speichert name und highscore des übergebendem Spielers
-	public void saveHighscore(Player player) throws IOException
-	{
-		FileOutputStream ioStream = new FileOutputStream(path);
-		ObjectOutputStream objStream = new ObjectOutputStream(ioStream);
-		objStream.writeObject(player.getName()+" " /* +player.countToken */);
-		objStream.close();
-		ioStream.close();
+
+	// Pfad zum speichern des Highscroes
+	private String path = "C:/Users/" + System.getProperty("user.name") + "/Documents/highscore.txt";
+	
+
+	public void print(/* Player player */) throws IOException {
+		PrintWriter out = new PrintWriter(new FileWriter(path, true));
+		out.println("3"/* player.getName()/*+highscore */);
+		out.close();
 	}
 
+	public void printHighscore() throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(path));
+
+		try {
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+			System.out.println(line);
+
+			while (line != null) {
+				sb.append(line);
+				sb.append(System.lineSeparator());
+				line = br.readLine();
+				if (line != null) {
+					System.out.println(line);
+				}
+			}
+			String everything = sb.toString();
+		} finally {
+			br.close();
+		}
+	}
 }
